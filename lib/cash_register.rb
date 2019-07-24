f15995a40ce76926712fce4bb77e9e43128388a3
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-24 02:50:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,7 +32,7 @@ attr_accessor :total, :discount, :quantity, :price, :item
         def items
           #returns an array containing all items that have been added
 
-          @@items << item
+          @@items << @item
 
         end
 
