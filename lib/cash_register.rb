e99793c45f1ed548c4b6e382c39e0daf90bfd0d4
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-24 02:27:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,17 +18,17 @@ attr_accessor :total, :discount, :quantity, :price, :items
         #binding.pry
 
         def apply_discount
-          #applies the discount to the total price
-          #returns success message with updated total
-          #reduces the total
-          #returns a string error message that there is no discount to apply
-        if @discount != 0
-          self.total = (@total * ((100 - @discount)/100)).to_i
-                return "After the discount, the total comes to $#{@total}."
-          else
-               return "There is no discount to apply."
-          end
-       end
+              #applies the discount to the total price
+              #returns success message with updated total
+              #reduces the total
+              #returns a string error message that there is no discount to apply
+            if discount != 0
+                self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+                    return "After the discount, the total comes to $#{@total}."
+              else
+                   return "There is no discount to apply."
+              end
+           end
 
         def items
           #returns an array containing all items that have been added
