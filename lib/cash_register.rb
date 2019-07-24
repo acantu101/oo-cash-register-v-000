4d111715d02496e1f780b0f022252bf7e1d9cb5d
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-24 03:11:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,11 +32,11 @@ attr_accessor :total, :discount, :quantity, :price, :elements
 
         def add_item(quantity = 0)
           #returns an array containing all items that have been added as many times as quantity expreses
-          i = 0
-          loop do
-          @items << @elements
-          i+=1
-          while i < quantity
+
+    
+          quantity.times {@items << @elements} times quantity
+          
+    
 
             end
 
